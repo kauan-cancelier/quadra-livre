5b4e7d80347bb9c53f266ff80704e3eccf9c82eb
Kauan Mello Cancelier <kauan.cancelier@nuntec.com.br>
2024-10-30 10:42:35 -0300

    Inicio do projeto.

---
+++ src/test/java/com/quadra_livre/quadra_livre/QuadraLivreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.quadra_livre.quadra_livre;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuadraLivreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
